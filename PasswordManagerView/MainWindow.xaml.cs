85d308ac14e72beec91fc59c6c2174b930b4d05e
AnVlMironov <AnVlMironov@Greenatom.ru>
2024-02-20 20:13:29 +0400

    Added libs for clean arch

---
+++ PasswordManagerView/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordManagerView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            WindowCreatingPassword window = new WindowCreatingPassword();
+            window.ShowDialog();
+        }
+    }
+}
